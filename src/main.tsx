5e2baec1f536a7d8f2ec28a4582b8a82419b1fbf
aw3s0m3ss <aw3s0m3ss@gmail.com>
2023-08-13 18:11:07 +0300

    feat: routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Home } from './pages/Home.tsx'
+import { Store } from './pages/Store.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,3 +13,26 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
+
+const router = createBrowserRouter([
+  {
+    path: "/react-types-store/",
+    element: <App />,
+    children: [
+      {
+        path: "/react-types-store/pages/Home",
+        element: <Home />,
+      },
+      {
+        path: "/react-types-store/pages/store",
+        element: <Store />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
