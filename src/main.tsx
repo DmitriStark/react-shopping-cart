7f36f06d1b93546e8dc8fb63444e317e59be6ef3
aw3s0m3ss <aw3s0m3ss@gmail.com>
2023-08-13 18:41:39 +0300

    fdsg

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { Home } from './pages/Home.tsx'
-import { Store } from './pages/Store.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -14,25 +12,28 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   </React.StrictMode>,
 )
 
-const router = createBrowserRouter([
-  {
-    path: "/react-types-store/",
-    element: <App />,
-    children: [
-      {
-        path: "/react-types-store/pages/Home",
-        element: <Home />,
-      },
-      {
-        path: "/react-types-store/pages/store",
-        element: <Store />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/react-types-store/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/react-types-store/pages/Home",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/react-types-store/pages/store",
+//         element: <Store />,
+//       },
+//     ],
+//   },
+// ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
+
+
+
